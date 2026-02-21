f3fb2853845b04ae8a2ea319ec12890b91b1379c
shruti02birari-lgtm <shruti02birari@gmail.com>
2026-02-21 19:21:58 +0530

    url displayed correctly

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/shrutibirari.github.io/',   // keep this exactly like this
+  base: '/',   // keep this exactly like this
 })
